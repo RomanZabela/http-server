a3ec7afd4cfaea68b74d365dfd6b0455fb426647
Roman Zabela <zaberoman@yandex.ru>
2022-09-11 00:45:14 +0300

    Adding update store

--- source/controllers/store.controllers.ts
+++ source/controllers/store.controllers.ts
@@ -73,4 +73,48 @@ const getStoreByID = async (req: Request, res: Response, next: NextFunction) => 
     }
 };
 
+const updateStore = async (req: Request, res: Response, next: NextFunction) => {
+    let id: number = -1;
+    let capacity: string = "";
+
+    const sId: string = req.params.id;
+
+    if (isNaN(Number(req.params.id))) {
+
+    }
+
+    if (sId !== null && sId !== undefined) {
+        id = parseInt(sId);
+    } else {
+
+    }
+
+    if (id > 0) {
+        storeService.getStore(id)
+        .then((result: Stores) => {
+            return res.status(200).json({
+                result
+            });
+        })
+        .catch((error: systemError) => {
+            switch (error.code) {
+                case ErrorCodes.ConnectionError:
+                    return res.status(408).json({
+                        errorMessage: error.message
+                    });
+                case ErrorCodes.queryError:
+                    return res.status(406).json({
+                        errorMessage: error.message
+                    });
+                default:
+                    return res.status(400).json({
+                        errorMessage: error.message
+                    });   
+            }
+        })
+    } else {
+
+    }
+};
+
 export default { getStores, getStoreByID };
